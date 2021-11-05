0c0ed2e246b22dd172102d783372c47b0b313a50
Matsuoka Taisei <79775965+taisei-Matsuoka@users.noreply.github.com>
2021-11-05 20:29:40 +0900

    Excelファイル
    Afterのファイルを一つにまとめ、ファイル名を変更

--- excel.py
+++ excel.py
@@ -1,6 +1,5 @@
 # %%
 import openpyxl
-from openpyxl.workbook import Workbook
 import glob
 import shutil
 from natsort import natsorted
@@ -13,14 +12,14 @@ sheet1_column = 1
 sheet2_row =1 
 sheet2_column=1
 column_list = ['A','B','C','D']
-files = glob.glob('C:/Users/USER/Desktop/excel/*.xlsx') #☆☆☆☆☆☆ファイルの変更場所①☆☆☆☆☆☆☆☆
+files = glob.glob('C:/Users/USER/Desktop/excel/*.xlsx')
 
-
+new_file = openpyxl.Workbook()
 
 for name in natsorted(files):#ファイルを上から順番に取得
     before = openpyxl.load_workbook(name)
     sheet1 = before['Sheet1']
-    new_file = openpyxl.Workbook()
+    
 
 
 
@@ -38,33 +37,38 @@ for name in natsorted(files):#ファイルを上から順番に取得
             syurui = result1 +"x"+result2
 
             after = new_file.active #新しく作ったファイルを利用可能に
-            for row_num in range(1,12):
+            for row_num in range(1,501):
                 after.row_dimensions[row_num].height = 50
-            for column_num in range(0,4):
+            for column_num in range(1,2):
                 after.column_dimensions[column_list[column_num]].width = 13
             
-            if sheet2_row % 11 ==0:
-                after.cell(sheet2_row,sheet2_column).value = syurui+'\n'+quanity #値設定
-                after.cell(sheet2_row,sheet2_column).alignment = openpyxl.styles.Alignment(wrapText=True)
-                sheet2_row = 1
-                sheet2_column += 1
-            else:
-                after.cell(sheet2_row,sheet2_column).value = syurui+'\n'+quanity
-                after.cell(sheet2_row,sheet2_column).alignment = openpyxl.styles.Alignment(wrapText=True)
-                sheet2_row += 1
+            after.cell(sheet2_row,1).value = syurui
+            after.cell(sheet2_row,2).value =   quanity
+            sheet2_row += 1
+            # if sheet2_row % 11 ==0:
+            #     after.cell(sheet2_row,sheet2_column).value = syurui+'\n'+quanity #値設定
+            #     after.cell(sheet2_row,sheet2_column).alignment = openpyxl.styles.Alignment(wrapText=True)
+            #     sheet2_row = 1
+            #     sheet2_column += 1
+
+            # else:
+            #     after.cell(sheet2_row,sheet2_column).value = syurui+'\n'+quanity
+            #     after.cell(sheet2_row,sheet2_column).alignment = openpyxl.styles.Alignment(wrapText=True)
+            #     sheet2_row += 1
             
         else:
+            # after = new_file.active
+            # after.cell(sheet2_row,1).value = '空白だよ～'
+            # after.cell(sheet2_row,2).value =   '空白だよ～'
+            # sheet2_row += 1
             pass
+    shutil.move(name,'C:/Users/USER/Desktop/excel/集計前')
 
-    after.page_margins.left = 0
-    after.page_margins.right = 0
-    after.page_margins.top = 0
-    after.page_margins.bottom = 0
-    after.page_margins.header = 0
-    after.page_margins.footer = 0
-    after.print_options.horizontalCentered = True
-    after.print_options.verticalCentered = True
+new_file.save('C:/Users/USER/Desktop/excel/集計後/集計.xlsx')
+    
+        
+# %%
+#%%
+import openpyxl
 
-    new_file.save('C:/Users/USER/Desktop/excel/after/'+number+'.xlsx')#☆☆☆☆☆☆ファイルの変更場所②☆☆☆☆☆☆☆☆
-    shutil.move(name,'C:/Users/USER/Desktop/excel/before')#☆☆☆☆☆☆ファイルの変更場所③☆☆☆☆☆☆☆☆
 # %%
